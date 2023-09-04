4a65e01eceeceb62bdf3fab9d2f7ca512d9aa0c8
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 14:30:33 +0200

    state with number

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useReducer } from 'react';
 import './App.scss';
 
-const initialState = 0;
+const initialState = {
+  number: 0
+};
+
+// const reducer = (state, action) => {
+//   const _state = {};
+
+//   return _state;
+// }
 
 function App() {
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
 
   return (
     <div>
       <h1>Reducer Todo List</h1>
-      <div>Number: {initialState}</div>
+      <div>Number: {initialState.number}</div>
     </div>
   )
 }
