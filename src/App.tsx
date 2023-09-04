f7f33541e17f71810573be63c75be554d6621a12
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 14:25:40 +0200

    init state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.scss'
+import './App.scss';
+
+const initialState = 0;
 
 function App() {
 
   return (
     <div>
       <h1>Reducer Todo List</h1>
+      <div>Number: {initialState}</div>
     </div>
   )
 }
