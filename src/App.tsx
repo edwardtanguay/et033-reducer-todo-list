0b001bd2f8e9b824ff7016a154405a7cc12dcf54
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 16:28:14 +0200

    basic test works

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,63 @@
 import { useReducer, useState } from 'react';
 import './App.scss';
 
+interface ITodo {
+	text: string;
+	mode: string;
+}
+
 interface IState {
-	todos: string[];
-	newTodo: string;
+	todos: ITodo[];
+	newTodoText: string;
+}
+
+interface IPayload {
+	index: number;
+	todo: ITodo;
+	newTodoText: string;
 }
 
 interface IAction {
 	type: string;
-	payload: string | number;
+	payload: number | string | ITodo;
 }
 
 const initialState: IState = {
 	todos: [],
-	newTodo: ''
+	newTodoText: ''
 };
 
 const reducer = (state: IState, action: IAction) => {
 	const _state = { ...state };
+	let index = 0;
 	switch (action.type) {
 		case 'addTodo':
-			if (typeof action.payload == 'string') {
-				const todo = action.payload;
+			if (typeof action.payload === 'string') {
+				const newTodoText = action.payload;
+				const todo: ITodo = {
+					text: newTodoText,
+					mode: 'normal'
+				}
 				_state.todos.push(todo);
-				_state.newTodo = '';
+				_state.newTodoText = '';
 			}
 			break;
 		case 'deleteTodo':
-			if (typeof action.payload == 'number') {
-				const index = action.payload;
+			if (typeof action.payload === 'number') {
+				index = action.payload;
 				_state.todos.splice(index, 1);
 			}
 			break;
+		case 'editTodo':
+			if (typeof action.payload === 'number') {
+				index = action.payload;
+				console.log(`editing todo index ${index}`);
+			}
+			break;
 		case 'changeNewTodo':
-			if (typeof action.payload == 'string') {
-				const changedTodo = action.payload;
-				_state.newTodo = changedTodo;
+			if (typeof action.payload === 'string') {
+				const newTodoText = action.payload;
+				_state.newTodoText = newTodoText;
 			}
 			break;
 	}
@@ -48,13 +70,13 @@ function App() {
 	return (
 		<div>
 			<h1>Reducer Todo List</h1>
-			<input value={state.newTodo} onChange={(e) => dispatch({ type: 'changeNewTodo', payload: e.target.value })} />{' '}
-			<button onClick={() => dispatch({ type: 'addTodo', payload: state.newTodo })}>Add Todo</button>
+			<input value={state.newTodoText} onChange={(e) => dispatch({ type: 'changeNewTodo', payload: e.target.value })} />{' '}
+			<button onClick={() => dispatch({ type: 'addTodo', payload: state.newTodoText })}>Add Todo</button>
 			<hr />
 			<div>There are {state.todos.length} todos:</div>
 			{state.todos.map((todo, index) => {
 				return (
-					<div key={index}>{todo} <button onClick={() => dispatch({ type: 'deleteTodo', payload: index })}>Delete</button></div>
+					<div key={index}>{todo.text} <button onClick={() => dispatch({ type: 'deleteTodo', payload: index })}>Delete</button> <button onClick={() => dispatch({ type: 'editTodo', payload: index })}>Edit</button></div>
 				)
 			})}
 		</div>
