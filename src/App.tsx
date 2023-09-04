8e2b0efee069432ef138248b2c42e1158aedf64e
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 15:46:58 +0200

    deleteTodo works

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,18 @@ const initialState: IState = {
 
 const reducer = (state: IState, action: IAction) => {
 	const _state = { ...state };
-	console.log(action);
 	switch (action.type) {
 		case 'addTodo':
-			if(typeof action.payload == 'string') _state.todos.push(action.payload);
+			if (typeof action.payload == 'string') {
+				const todo = action.payload;
+				_state.todos.push(todo);
+			}
 			break;
 		case 'deleteTodo':
-			console.log(`deleting todo with index ${action.payload}`);
+			if (typeof action.payload == 'number') {
+				const index = action.payload;
+				_state.todos.splice(index, 1);
+			}
 			break;
 	}
 	return _state;
@@ -30,19 +35,18 @@ const reducer = (state: IState, action: IAction) => {
 
 function App() {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const [ todo, setTodo ] = useState('');
-
+	const [todo, setTodo] = useState('');
 
 	return (
 		<div>
 			<h1>Reducer Todo List</h1>
-			<input value={todo} onChange={(e) => setTodo(e.target.value)}/>
-			<div><button onClick={() => dispatch({type: 'addTodo', payload:todo})}>Add Todo</button></div>
-			<hr/>
+			<input value={todo} onChange={(e) => setTodo(e.target.value)} />
+			<div><button onClick={() => dispatch({ type: 'addTodo', payload: todo })}>Add Todo</button></div>
+			<hr />
 			<div>There are {state.todos.length} todos:</div>
 			{state.todos.map((todo, index) => {
 				return (
-					<div key={index}>{todo} <button onClick={() => dispatch({ type: 'deleteTodo', payload: index})}>Delete</button></div>
+					<div key={index}>{todo} <button onClick={() => dispatch({ type: 'deleteTodo', payload: index })}>Delete</button></div>
 				)
 			})}
 		</div>
