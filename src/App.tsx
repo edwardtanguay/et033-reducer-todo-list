8ad5414bc53e532d669b6a9f918927ba8128d1be
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 15:25:55 +0200

    adding to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import './App.scss';
 
 interface IState {
-  count: number;
-  logMessage: string;
+	todos: string[];
+}
+
+interface IAction {
+	type: string;
+	payload: string;
 }
 
 const initialState: IState = {
-  count: 0,
-  logMessage : 'no logs yet'
+	todos: []
 };
 
-const reducer = (state: IState, action: string) => {
-  const _state = {...state};
-  switch (action) {
-    case 'change888':
-      _state.count = 888;
-      _state.logMessage = 'change to 888';
-      break;
-    case 'change999':
-      _state.count = 999;
-      _state.logMessage = 'change to 999';
-      break;
-  }
-  return _state;
+const reducer = (state: IState, action: IAction) => {
+	const _state = { ...state };
+	switch (action.type) {
+		case 'addTodo':
+			_state.todos.push(action.payload);
+	}
+	return _state;
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const [ todo, setTodo ] = useState('');
 
 
-  return (
-    <div>
-      <h1>Reducer Todo List</h1>
-      <div>Number: {state.count}</div>
-      <div>Log message: {state.logMessage}</div>
-      <div><button onClick={() => dispatch("change888")}>Change to 888 count</button></div>
-      <div><button onClick={() => dispatch("change999")}>Change to 999 count</button></div>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Reducer Todo List</h1>
+			<input value={todo} onChange={(e) => setTodo(e.target.value)}/>
+			<div><button onClick={() => dispatch({type: 'addTodo', payload:todo})}>Add Todo</button></div>
+			<hr/>
+			<div>There are {state.todos.length} todos:</div>
+			{state.todos.map(todo => {
+				return (
+					<div>{todo}</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default App
