10c495882bfc7cbd317809c10424c04ae75c6bd9
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 15:59:55 +0200

    todo in state now

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 
 interface IState {
 	todos: string[];
+	todo: string;
 }
 
 interface IAction {
@@ -11,7 +12,8 @@ interface IAction {
 }
 
 const initialState: IState = {
-	todos: []
+	todos: [],
+	todo: ''
 };
 
 const reducer = (state: IState, action: IAction) => {
@@ -21,6 +23,7 @@ const reducer = (state: IState, action: IAction) => {
 			if (typeof action.payload == 'string') {
 				const todo = action.payload;
 				_state.todos.push(todo);
+				_state.todo = '';
 			}
 			break;
 		case 'deleteTodo':
@@ -29,19 +32,24 @@ const reducer = (state: IState, action: IAction) => {
 				_state.todos.splice(index, 1);
 			}
 			break;
+		case 'changeTodo':
+			if (typeof action.payload == 'string') {
+				const changedTodo = action.payload;
+				_state.todo = changedTodo;
+			}
+			break;
 	}
 	return _state;
 }
 
 function App() {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const [todo, setTodo] = useState('');
 
 	return (
 		<div>
 			<h1>Reducer Todo List</h1>
-			<input value={todo} onChange={(e) => setTodo(e.target.value)} />
-			<div><button onClick={() => dispatch({ type: 'addTodo', payload: todo })}>Add Todo</button></div>
+			<input value={state.todo} onChange={(e) => dispatch({ type: 'changeTodo', payload: e.target.value })} />
+			<div><button onClick={() => dispatch({ type: 'addTodo', payload: state.todo })}>Add Todo</button></div>
 			<hr />
 			<div>There are {state.todos.length} todos:</div>
 			{state.todos.map((todo, index) => {
