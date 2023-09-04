5c3c15b8363fd793d267e36213cc54bdc888c1d7
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-04 15:03:50 +0200

    the simplest example of useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import { useReducer } from 'react';
 import './App.scss';
 
-const initialState = {
-  number: 0
+interface IState {
+  count: number
+}
+
+const initialState: IState = {
+  count: 0
 };
 
-// const reducer = (state, action) => {
-//   const _state = {};
-
-//   return _state;
-// }
+const reducer = (state: IState, action: string) => {
+  const _state = {...state};
+  switch (action) {
+    case 'change888':
+      _state.count = 888;
+      break;
+    case 'change999':
+      _state.count = 999;
+      break;
+  }
+  return _state;
+}
 
 function App() {
-  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
 
   return (
     <div>
       <h1>Reducer Todo List</h1>
-      <div>Number: {initialState.number}</div>
+      <div>Number: {state.count}</div>
+      <div><button onClick={() => dispatch("change888")}>Change to 888 count</button></div>
+      <div><button onClick={() => dispatch("change999")}>Change to 999 count</button></div>
     </div>
   )
 }
